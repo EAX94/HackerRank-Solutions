729517bb4324af83276fdc051bec48fcca39eee8
LUCAS <kiskegl@gmail.com>
2020-09-29 23:22:23 -0300

    Create Solution.cs

---
+++ Interview Preparation Kit/Arrays/Minimum Swaps 2/Solution.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int result = 0;
+        int arrLength = arr.Length;
+        int i = 0;
+
+        while(i < arrLength){
+            if(arr[i] != (i+1)){
+                int tempNum = arr[i];
+
+                arr[i] = arr[tempNum - 1];
+                arr[tempNum - 1] = tempNum;
+
+                ++result;
+            } else{
+                ++i;
+            }
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
